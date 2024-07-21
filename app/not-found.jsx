03c94bb99f6bf25c035e07768ecbc5b4ed619582
Elda Moudy <elda@moudy.net>
2024-07-20 22:43:57 -0400

    adding not-found pages

---
+++ app/not-found.jsx
@@ -0,0 +1,15 @@
+// this file has to be named 'not-found.jsx' to be picked up by the router
+
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="text-center">
+      <h2 className="text-3xl">There was a problem.</h2>
+      <p className="text-lg">The page you are looking for does not Exist</p>
+      <p>
+        Go back to the <Link href="/">Dashboard</Link>
+      </p>
+    </main>
+  );
+}
