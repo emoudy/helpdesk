31ef9ba4fcce3a0d1b8e23396a2c5a5ab8ab730c
Elda Moudy <elda@moudy.net>
2024-07-20 21:37:00 -0400

    adding images and css

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
+import picture from "../../public/sun.png";
 
 import React from "react";
 
 export default function NavBar() {
   return (
     <nav>
+      <Image
+        src={picture}
+        alt="splash logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>HelpDesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
