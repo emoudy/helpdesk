a06fecf060196bd46fac1e7207de77e656709f62
Elda Moudy <elda@moudy.net>
2024-07-20 20:35:45 -0400

    adding pages

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <h1>HelpDesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  );
+}
