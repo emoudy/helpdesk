578eeeea09e0c282e488abc41e214988dda284f7
Elda Moudy <elda@moudy.net>
2024-07-26 22:50:30 -0400

    removing setTimeout

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -33,7 +33,7 @@ export async function generateStaticParams() {
 
 async function getTicket(id) {
   // imitate the delay of a real API call
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
 
   const res = await fetch("http://localhost:4000/tickets/" + id, {
     next: {
