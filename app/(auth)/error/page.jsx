89063e39fa9bfd42ccaff5a7dd2726a24dc51be0
Elda Moudy <elda@moudy.net>
2024-07-31 16:20:20 -0400

    creating an error page

---
+++ app/(auth)/error/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function ErrorPage() {
+  return (
+    <main className="text-center">
+      <h2 className="text-3xl">There was a problem.</h2>
+      <p>
+        Go back to the <Link href="/login">Login Page</Link>
+      </p>
+    </main>
+  );
+}
