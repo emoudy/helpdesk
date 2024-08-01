94682bba91fc65c7b9126f975a9e69bca3bcc089
Elda Moudy <elda@moudy.net>
2024-08-01 11:15:50 -0400

    fixing login flow

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -39,7 +39,8 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/error') &&
-    !request.nextUrl.pathname.startsWith('/confirm')
+    !request.nextUrl.pathname.startsWith('/confirm') &&
+    !request.nextUrl.pathname.startsWith('/verify')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
