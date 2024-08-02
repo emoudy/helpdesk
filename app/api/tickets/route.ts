f4c1146ded5f88aeb8658ff1ee6cc1b8d4a692a5
Elda Moudy <elda@moudy.net>
2024-08-02 14:20:12 -0400

    adding typescript and fixing routes

---
+++ app/api/tickets/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { createClient } from '../../../utils/supabase/server';
+
+/**
+ * This forces all the route handlers to be dynamic.  They are static by default.
+ * By setting this to "force-dynamic", they will be dynamic and fetch anytime a request is made.
+ */
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  const supabase = createClient();
+  const { data, error } = await supabase.from('Tickets')
+    .select()
+
+  if (error) {
+    console.log(error.message)
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(data);
+}
+
+export async function POST(request: NextRequest) {
+  const ticket = await request.json();
+  const supabase = createClient();
+  const { data: { user }, error: userError } = await supabase.auth.getUser();
+
+  if (userError || !user) {
+    console.error("Error fetching user:", userError?.message || "User not found");
+    return NextResponse.json({ error: userError?.message || "User not found" }, { status: 401 });
+  }
+
+  const { data, error } = await supabase.from('Tickets').insert({
+    ...ticket,
+    user_email: user.email,
+  }).select().single();
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ data });
+}
