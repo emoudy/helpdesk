8fc569c4793142e651d21d41f9f23bd22815010c
Elda Moudy <elda@moudy.net>
2024-08-07 18:08:38 -0400

    removing the Login link

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,11 +1,8 @@
-import Link from "next/link";
-
 export default function AuthLayouts({ children }) {
   return (
     <>
       <nav>
         <h1>Helpdesk</h1>
-        <Link href="/login">Log In</Link>
       </nav>
       {children}
     </>
