c644ee2b4abec15530dcc112bd003286caa82dc4
Elda Moudy <elda@moudy.net>
2024-08-06 23:46:02 -0400

    changing the link of create new ticket

--- app/(dashboard)/tickets/page.jsx
+++ app/(dashboard)/tickets/page.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import TicketList from "./TicketList";
 import Loading from "../loading";
+import Link from "next/link";
 
 export const metadata = {
   title: "Helpdesk | Tickets",
@@ -15,10 +16,15 @@ export default function Tickets() {
     <main>
       <nav>
         <div>
-          <h2>Tickets Page</h2>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
         </div>
+        <Link href="/tickets/create" className="ml-auto">
+          <button className="btn-primary">New Ticket</button>
+        </Link>
       </nav>
-
       <Suspense fallback={<Loading />}>
         <TicketList />
       </Suspense>
