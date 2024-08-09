84082f2b11562ee09f133a5b157e3cbab3c49d13
Elda Moudy <elda@moudy.net>
2024-08-08 22:14:36 -0400

    Creating EditIcon

---
+++ app/components/EditTicketIcon.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { FaRegEdit } from "react-icons/fa";
+import Link from "next/link";
+
+export default function EditTicketIcon({ id }) {
+  return (
+    <Link href={`/tickets/${id}/edit`}>
+      <FaRegEdit className="icon" />
+    </Link>
+  );
+}
