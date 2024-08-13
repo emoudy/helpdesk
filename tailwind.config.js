b569a5f05002bfb6782e077ac5209d35c7090ad6
Elda Moudy <elda@moudy.net>
2024-08-13 15:30:16 -0400

    adding standard fontSize for the application

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#6744FF',
+        neutral: '#D0D0D0', // light grey
+        primary: '#6744FF', // purple
+        secondary: '#CC9A33', // soft gold
         primaryDark: '#4D25E5', // 10% darker than primary
+      },
+      fontSize: {
+        'button-sm': ['12px', '16px'], // small button text size with line-height
+        'button-md': ['14px', '20px'], // medium button text size with line-height
+        'button-lg': ['16px', '24px'], // large button text size with line-height
       },
     },
   },
