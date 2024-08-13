ee8a7d887b440d2dadc9197959d0efeac5c83458
Elda Moudy <elda@moudy.net>
2024-08-13 00:49:39 -0400

    fixing css for the columns

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -18,12 +18,12 @@ export default async function DashboardLayout({ children }: DashboardLayoutProps
   } = await supabase.auth.getUser();
 
   return (
-    <div className="help_desk_app grid h-screen grid-cols-5 grid-rows-1 bg-white">
-      <header className="col-span-1 row-span-2 flex h-full flex-col bg-primary p-4 text-white">
+    <div className="help_desk_app grid h-screen grid-cols-[11rem_1fr] grid-rows-[1fr_auto] bg-white">
+      <header className="row-span-2 flex h-full flex-col bg-primary p-4 text-white w-44">
         <NavBar user={user as User} />
       </header>
-      <main className="col-span-4 m-10 overflow-y-auto">{children}</main>
-      <footer className="col-span-4 self-end py-2 text-center text-sm text-primary text-opacity-50">
+      <main className="m-10 overflow-y-auto">{children}</main>
+      <footer className="flex items-center justify-center py-2 text-center text-sm text-primary text-opacity-50">
         @moudy2024
       </footer>
     </div>
