a69504de11878fb26bf3df2fd07abe43d025bbfb
Elda Moudy <elda@moudy.net>
2024-08-13 11:13:11 -0400

    fixing the padding on the Navigation Links

--- app/(dashboard)/components/navbar/NavBarLink.tsx
+++ app/(dashboard)/components/navbar/NavBarLink.tsx
@@ -3,10 +3,11 @@ import Link from 'next/link';
 export default function NavBarLink({ linkName, isActive, href }) {
   return (
     <Link
-      className={`nav-link flex flex-col rounded-lg px-4 py-2 hover:bg-primaryDark ${isActive ? 'active bg-primaryDark' : ''}`}
+      className={`nav-link flex flex-col my-2`}
       href={href}
-    >
+    ><h2 className={`text-white px-4 py-2 hover:bg-primaryDark rounded-lg ${isActive ? 'active bg-primaryDark' : ''}`}>
       {linkName}
+    </h2>
     </Link>
   );
 }
