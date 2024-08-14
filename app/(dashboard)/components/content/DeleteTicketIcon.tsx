5fc2edf767a6daf391b51d326d43ab316a900dd7
Elda Moudy <elda@moudy.net>
2024-08-13 22:18:14 -0400

    deleting extra spaces

--- app/(dashboard)/components/content/DeleteTicketIcon.tsx
+++ app/(dashboard)/components/content/DeleteTicketIcon.tsx
@@ -29,7 +29,7 @@ export default function DeleteTicketIcon({ id }: DeleteTicketIconProps) {
   return (
     <TbTrashFilled
       className="icon"
-      onClick={handleDelete}   
+      onClick={handleDelete}
     />
   );
 }
