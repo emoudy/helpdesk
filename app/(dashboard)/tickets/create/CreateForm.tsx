f9c5cc7c1514425bca81ced913192e3e39927ad1
Elda Moudy <elda@moudy.net>
2024-08-13 22:19:11 -0400

    putting header at page level and adding suspense

--- app/(dashboard)/tickets/create/CreateForm.tsx
+++ app/(dashboard)/tickets/create/CreateForm.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import ContentHeader from '../../components/content/ContentHeader';
 import TicketForm from '../../components/content/TicketForm';
 
 export default function CreateForm() {
   return (
-    <>
-      <ContentHeader headerTitle="Create Ticket" href="/tickets" nextPage="Ticket List" />
-      <TicketForm ticket={null} action="Create" />
-    </>
+    <TicketForm ticket={null} action="Create" />
   );
 }
--- app/(dashboard)/tickets/create/page.tsx
+++ app/(dashboard)/tickets/create/page.tsx
@@ -1,9 +1,16 @@
+import { Suspense } from 'react';
+
 import CreateForm from './CreateForm';
+import ContentHeader from '../../components/content/ContentHeader';
+import Loading from '../../loading';
 
 export default function CreateTicket() {
   return (
     <main>
-      <CreateForm />
+      <ContentHeader headerTitle="Create Ticket" href="/tickets" nextPage="Ticket List" />
+      <Suspense fallback={<Loading />}>
+        <CreateForm />
+      </Suspense>
     </main>
   );
 }
