b0de10bc18f6e7162e360791db01606f5f563203
Elda Moudy <elda@moudy.net>
2024-08-17 18:31:38 -0400

    Adding Footer component

---
+++ app/(dashboard)/components/content/footer/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  /*We're using z-0 on the CSS of the Footer because on the component EditTicket, the QuillEditor goes over the footer*/
+  return (
+    <footer className="z-0 row-start-4 col-start-2 flex items-center justify-center py-2 text-sm text-primary bg-white text-opacity-50">
+      @moudy2024
+    </footer>
+  )
+}
