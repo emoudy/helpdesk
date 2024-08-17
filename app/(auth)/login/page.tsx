226a766140ae46630983a3ab3b671826a9851eeb
Elda Moudy <elda@moudy.net>
2024-08-17 18:32:57 -0400

    changing html tags to keep a consistent structure

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,9 +3,8 @@ import MainHeader from '@/(dashboard)/components/header/MainHeader';
 
 export default function Login() {
   return (
-    <main className="col-span-2 max-w-96 mx-auto m-10">
-      <MainHeader />
+    <div className="max-w-md mx-auto m-10">
       <AuthForm />
-    </main>
+    </div>
   );
 }
--- app/(auth)/verify/page.tsx
+++ app/(auth)/verify/page.tsx
@@ -1,11 +1,11 @@
 export default function Verify() {
   return (
-    <main className="text-center mt-10">
+    <div className="text-center mt-10">
       <h2>Thanks for registering!</h2>
       <p className="my-5">
         Before logging in, you need to verify your email address. Check your
         email to confirm the account.
       </p>
-    </main>
+    </div>
   );
 }
