043cc78cab5d78c4622af6a7234f8882f075fa2f
Elda Moudy <elda@moudy.net>
2024-08-17 00:54:58 -0400

    Adding DashboardHeader to the ContentHeader

--- app/(dashboard)/components/content/ContentHeader.tsx
+++ app/(dashboard)/components/content/ContentHeader.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { MdOutlineKeyboardBackspace } from 'react-icons/md';
+import DashboardHeader from './DashboardHeader';
 
 interface ContentHeaderProps {
   headerTitle: string,
@@ -9,14 +10,14 @@ interface ContentHeaderProps {
 
 export default function ContentHeader({ headerTitle, href, nextPage}: ContentHeaderProps) {
   return (
-    <nav>
-      <h1 className="mb-5">{headerTitle}</h1>
+    <>
+      <DashboardHeader headerTitle={headerTitle} />
       <Link href={href} className='hidden md:block'>
         <button className="small-btn btn-primary md:mb-20 flex items-center">
           <MdOutlineKeyboardBackspace />
           &nbsp; Back to {nextPage}
         </button>
       </Link>
-    </nav>
+    </>
   )
 }
