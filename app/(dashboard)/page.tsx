894f83b16fbf01ba2b38fbf66dfb48f11aa1ad4b
Elda Moudy <elda@moudy.net>
2024-08-17 18:32:07 -0400

    adding proper html tags to the Dashboad page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,16 +3,18 @@ import DashboardHeader from './components/content/DashboardHeader';
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <DashboardHeader headerTitle={"Dashboard"} />
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero
-        repellendus tempore, exercitationem odit, quasi doloremque possimus
-        recusandae alias sequi totam soluta natus iure eius, obcaecati sint
-        dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed
-        voluptates iste cum totam, facere explicabo, fugit suscipit ratione
-        aspernatur consequuntur ex mollitia quaerat?
-      </p>
+      <section>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero
+          repellendus tempore, exercitationem odit, quasi doloremque possimus
+          recusandae alias sequi totam soluta natus iure eius, obcaecati sint
+          dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed
+          voluptates iste cum totam, facere explicabo, fugit suscipit ratione
+          aspernatur consequuntur ex mollitia quaerat?
+        </p>
+      </section>
 
       <div className="my-8 flex justify-center">
         <Link href="/tickets">
@@ -20,27 +22,31 @@ export default function Home() {
         </Link>
       </div>
 
-      <h1>Company Updates</h1>
+      <article>
+        <h1>Company Updates</h1>
+        <section>
+          <div className="mb-5">
+            <h2>New member of the web dev team...</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at
+              quam. Dolores omnis possimus quam soluta rerum illo laborum ullam
+              pariatur molestiae, modi beatae corrupti.
+            </p>
+          </div>
+        </section>
 
-      <div className="mb-5">
-        <h2>New member of the web dev team...</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at
-          quam. Dolores omnis possimus quam soluta rerum illo laborum ullam
-          pariatur molestiae, modi beatae corrupti.
-        </p>
-      </div>
-      <div className="mb-5">
-        <h2>New website live!</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at
-          quam. Dolores omnis possimus quam soluta rerum illo laborum ullam
-          pariatur molestiae, modi beatae corrupti, assumenda distinctio
-          adipisci, cupiditate minima eum vitae? Similique dicta est facilis
-          debitis, autem temporibus quo repellat illum unde id iste veritatis
-          eveniet, aspernatur enim quas.
-        </p>
-      </div>
-    </main>
+        <section className="mb-5">
+          <h2>New website live!</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at
+            quam. Dolores omnis possimus quam soluta rerum illo laborum ullam
+            pariatur molestiae, modi beatae corrupti, assumenda distinctio
+            adipisci, cupiditate minima eum vitae? Similique dicta est facilis
+            debitis, autem temporibus quo repellat illum unde id iste veritatis
+            eveniet, aspernatur enim quas.
+          </p>
+        </section>
+      </article>
+    </div>
   );
 }
