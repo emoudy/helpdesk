a612d1a1b5cd41d9210ab22e3ba8eb0aa163c2bd
Elda Moudy <elda@moudy.net>
2024-08-17 18:33:16 -0400

    structuring the layout for consistency

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from 'react';
+import MainHeader from '@/(dashboard)/components/header/MainHeader';
 
 interface AuthLayoutsProps {
   children: ReactNode;
 }
 
 export default function AuthLayouts({ children }: AuthLayoutsProps) {
-  return <>{children}</>;
+  return (
+    <div>
+      <header className='md:mt-10'>
+        <MainHeader />
+      </header>
+      <main aria-label="Main Content">{children}</main>
+    </div>);
 }
