e0e9270d128385f9c7468181af498dd78db07154
Elda Moudy <elda@moudy.net>
2024-08-16 21:51:11 -0400

    adding divs to structure the forms

--- app/(auth)/AuthForm.tsx
+++ app/(auth)/AuthForm.tsx
@@ -3,10 +3,14 @@ import { login, signup } from './actions';
 export default function AuthForm() {
   return (
     <form id="formElem">
-      <label htmlFor="email">Email:</label>
-      <input id="email" type="email" name="email" required aria-label="Form Email" />
-      <label htmlFor="password">Password:</label>
-      <input id="password" type="password" name="password" required aria-label="Form Password" />
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" required aria-label="Form Email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" required aria-label="Form Password" />
+      </div>
       <div className="text-center">
         <button className="btn-primary mb-2" formAction={login}>
           Login
