bf7ba3180b9ac3742064b34429b036b3c873ea1a
Elda Moudy <elda@moudy.net>
2024-08-16 21:54:39 -0400

    Cleaning out NavBar with components

--- app/(dashboard)/components/navbar/NavBar.tsx
+++ app/(dashboard)/components/navbar/NavBar.tsx
@@ -1,54 +1,29 @@
-import Image from 'next/image';
-import picture from '@public/sun_small.png';
-import LogoutButton from './LogoutButton';
-import NavBarLinkList from './NavBarLinkList';
+'use client';
 
-interface NavBarProps {
-  user: { email: string };
-}
+import NavBarLink from './NavBarLink';
+import { usePathname } from 'next/navigation';
 
-export default function NavBar({ user }: NavBarProps) {
-  const LogoSection = () =>
-    <div className="flex flex-col items-center md:text-left">
-      <Image
-        src={picture}
-        alt="HelpDesk logo"
-        width={70}
-        quality={100}
-        placeholder="blur"
-      />
-      <h3 className="md:mb-4 md:mt-2 font-semibold text-inherit">
-        HelpDesk
-      </h3>
-    </div>;
+export default function NavBar() {
+  const dashboardLink = '/';
+  const ticketsLink = '/tickets';
 
-  const DesktopChildren = () =>
-    <div className="flex flex-col h-full">
-      <div className="flex flex-col items-center border-b-2 border-white">
-        <LogoSection />
-        <small className="pb-4">{user ? user.email : 'null'}</small>
-      </div>
-      <NavBarLinkList />
-      <LogoutButton />
-    </div>;
-
-  const MobileChildren = () =>
-    <>
-      <div className="flex justify-between items-center">
-        <LogoSection />
-        <LogoutButton />
-      </div>
-      <NavBarLinkList />
-    </>;
+  const linkRoutes = [ticketsLink];
+  const pathname = usePathname();
+  const activeLink =
+    linkRoutes.find(link => pathname.includes(link)) || dashboardLink;
 
   return (
-    <nav className="h-full text-white" aria-label="Main navigation">
-      <div className="hidden md:block h-full">
-        <DesktopChildren />
-      </div>
-      <div className='md:hidden'>
-        <MobileChildren />
-      </div>
-    </nav>
+    <>
+      <NavBarLink
+        linkName="Dashboard"
+        isActive={activeLink === dashboardLink}
+        href={dashboardLink}
+      />
+      <NavBarLink
+        linkName="Tickets"
+        isActive={activeLink === ticketsLink}
+        href={ticketsLink}
+      />
+    </>
   );
 }
