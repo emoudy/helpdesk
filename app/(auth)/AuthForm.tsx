8a73fa17afe826714e714877ad4eb9481811bec7
Elda Moudy <elda@moudy.net>
2024-08-17 00:52:20 -0400

    CSS to Login page

--- app/(auth)/AuthForm.tsx
+++ app/(auth)/AuthForm.tsx
@@ -4,19 +4,19 @@ export default function AuthForm() {
   return (
     <form id="formElem">
       <div>
-        <label htmlFor="email">Email:</label>
+        <label htmlFor="email" className='text-white'>Email</label>
         <input id="email" type="email" name="email" required aria-label="Form Email" />
       </div>
       <div>
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password" className='text-white'>Password</label>
         <input id="password" type="password" name="password" required aria-label="Form Password" />
       </div>
       <div className="text-center">
         <button className="btn-primary" formAction={login}>
           Login
         </button>
-        <div className="my-2">or</div>
-        <button className="btn-secondary" formAction={signup}>
+        <div className="my-2 font-bold">or</div>
+        <button className="btn-secondary border-primary border-2" formAction={signup}>
           Sign Up
         </button>
       </div>
