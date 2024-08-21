88016e45a08b2c3532218196eeada5fee963cb2b
Elda Moudy <elda@moudy.net>
2024-08-21 18:56:40 -0400

    typo

--- app/(dashboard)/tickets/create/page.tsx
+++ app/(dashboard)/tickets/create/page.tsx
@@ -7,15 +7,15 @@ import Loading from '../../loading';
 export default function CreateTicket() {
   const crumbs = [{name:"Ticket List", href:"/tickets"}, {name:"Create Ticket", href:""}];
   return (
-    <main>
+    <>
       <header>
         <ContentHeader crumbs={crumbs} />
       </header>
-      <main className='flex flex-col items-center'>
+      <article className='flex flex-col items-center'>
         <Suspense fallback={<Loading />}>
           <CreateForm />
         </Suspense>
-      </main>
-    </main>
+      </article>
+    </>
   );
 }
