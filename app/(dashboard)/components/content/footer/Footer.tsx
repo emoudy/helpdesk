c1913ea1adb19769ce6397520346090f46a1dabf
Elda Moudy <elda@moudy.net>
2024-08-21 17:26:45 -0400

    CSS for Footer

--- app/(dashboard)/components/content/footer/Footer.tsx
+++ app/(dashboard)/components/content/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
-  /*We're using z-0 on the CSS of the Footer because on the component EditTicket, the QuillEditor goes over the footer*/
   return (
-    <div 
+    <div
+      className="text-sm text-primary bg-white text-opacity"
       aria-label="Footer">
       @moudy2024
     </div>
