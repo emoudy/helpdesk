282b85f4af04a764da01559eeb03fe3303a1990a
Elda Moudy <elda@moudy.net>
2024-08-21 18:11:03 -0400

    structure for Auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,13 +8,10 @@ interface AuthLayoutsProps {
 
 export default function AuthLayouts({ children }: AuthLayoutsProps) {
   return (
-    <div className='md:flex flex-col justify-center bg-white'>
+    <>
       <header>
         <MainHeader />
       </header>
       <main aria-label="Main Content">{children}</main>
-      <footer className="z-0 row-start-4 col-start-2 text-center py-2 text-sm text-primary bg-white text-opacity-50">
-        <Footer />
-      </footer>
-    </div>);
+    </>);
 }
