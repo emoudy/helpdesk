ddfa169e17ddcbd217f6ea940767e6c4bcf44422
Elda Moudy <elda@moudy.net>
2024-08-21 10:00:38 -0400

    using MainHeader with small of large icons

--- app/(dashboard)/components/header/MainHeader.tsx
+++ app/(dashboard)/components/header/MainHeader.tsx
@@ -1,20 +1,32 @@
 import Image from 'next/image';
 import React from 'react'
-import picture from '@public/sun_small.png';
+import smPicture from '@public/sun_small.png';
+import lgPicture from '@public/sun.png';
 import LogoutIcon from '../navbar/LogoutIcon';
 
-export default function MainHeader() {
+interface MainHeaderProps {
+  small?: boolean;
+};
+
+export default function MainHeader({ small }: MainHeaderProps) {
+  const logoSize = {
+    width: small ? 70 : 170,
+    pic: small ? smPicture : lgPicture,
+    textSize: small ? 'null' : 'text-4xl',
+    textColor: small ? 'text-white' : 'text-primary',
+  }
+
   return (
     <div className="flex justify-center items-center p-5">
       <section className="flex flex-col justify-center items-center">
         <Image
-          src={picture}
+          src={logoSize.pic}
           alt="HelpDesk logo"
-          width={70}
+          width={logoSize.width}
           quality={100} 
           placeholder="blur"
         />
-        <h3 className="text-white">
+        <h3 className={`${logoSize.textColor} ${logoSize.textSize}`}>
           HelpDesk
         </h3>
       </section>
