b62d7c45ea61c5b250bdb952519466e5b188bfa8
Elda Moudy <elda@moudy.net>
2024-08-20 20:26:36 -0400

    changing ReactIcons for Heroicons

--- app/(dashboard)/components/content/Breadcrumbs.tsx
+++ app/(dashboard)/components/content/Breadcrumbs.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { IoIosArrowForward } from "react-icons/io";
 
 interface Breadcrumb {
   name: string;
@@ -20,7 +19,9 @@ export default function Breadcrumbs({ crumbs }: BreadcrumbsProps) {
           </Link>
           &nbsp;
           {index < crumbs.length - 1 && (
-            <IoIosArrowForward />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+            </svg>    
           )}
           {index === crumbs.length - 1 && <span className='text-gray-700'>{crumb.name}</span>}
         </div>
--- app/(dashboard)/components/navbar/LogoutIcon.tsx
+++ app/(dashboard)/components/navbar/LogoutIcon.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from 'next/navigation';
 import { createClient } from '@utils/supabase/client';
-import { IoMdLogOut } from "react-icons/io";
 
 export default function LogoutIcon() {
   const router = useRouter();
@@ -19,6 +18,8 @@ export default function LogoutIcon() {
   };
 
   return (
-    <IoMdLogOut className="md:hidden icon text-white" onClick={handleLogout} />
+    <svg onClick={handleLogout} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 md:hidden actionIcon text-white">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+    </svg>
   );
 }
