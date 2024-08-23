294b9d352552b5de4763fd405d3e5854867cafd9
Elda Moudy <elda@moudy.net>
2024-08-23 08:44:58 -0400

    showing error message

--- app/(dashboard)/_components/navbar/LogoutButton.tsx
+++ app/(dashboard)/_components/navbar/LogoutButton.tsx
@@ -5,21 +5,22 @@ import { createClient } from '@utils/supabase/client';
 
 export default function LogoutButton() {
   const router = useRouter();
+  let hasLogoutError = false;
 
   const handleLogout = async () => {
     const supabase = createClient();
     const { error } = await supabase.auth.signOut();
 
-    if (!error) {
-      router.push('/login');
-    } else {
-      console.log('error', error);
-    }
+    if (!error) router.push('/login');
+    else hasLogoutError = true;
   };
 
   return (
-    <button className="small-btn btn-primary_alt mb-2" onClick={handleLogout}>
-      Logout
-    </button>
+    <div className='text-center'>
+      { hasLogoutError ? <p className='text-sm text-red-800 font-semibold mb-2'>There was an error logging out.</p> : null}
+      <button className="small-btn btn-primary_alt mb-2" onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
   );
 }
