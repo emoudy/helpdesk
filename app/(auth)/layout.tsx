e9b1ea8bbf769348a14288ce4f4a9955856279a0
Elda Moudy <elda@moudy.net>
2024-08-23 01:25:58 -0400

    moving a parenthesis

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -14,5 +14,6 @@ export default function AuthLayouts({ children }: AuthLayoutsProps) {
         <MainHeader size={large} />
       </header>
       <main aria-label="Main Content">{children}</main>
-    </>);
+    </>
+  );
 }
