f1369d841f6e2b8d0dfed5167227bc8a8ef907f0
Elda Moudy <elda@moudy.net>
2024-08-29 16:58:48 -0400

    removing import

--- app/(dashboard)/_components/form/formButtons/SaveTicketButton.tsx
+++ app/(dashboard)/_components/form/formButtons/SaveTicketButton.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import LoadingIcon from "@dashboard/_components/icons/LoadingIcon";
-import Tooltip from "./Tooltip";
 
 interface SaveTicketButtonProps {
   actionState: { isLoading: boolean, error: boolean };
 }
 export const SaveTicketButton = ({ actionState }: SaveTicketButtonProps) => {
-
   return (
       <button type="submit" className="medium-btn btn-primary mb-5" disabled={actionState.isLoading}>
         {actionState.isLoading ? (
