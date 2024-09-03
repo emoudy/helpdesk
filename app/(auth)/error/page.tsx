701883f3b5fbab9ae4405449d14279f45ca3b32a
Elda Moudy <elda@moudy.net>
2024-09-03 10:38:55 -0400

    fixing css in error page

--- app/(auth)/error/page.tsx
+++ app/(auth)/error/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function ErrorPage() {
   return (
-    <div className='flex flex-col items-center justify-center w-1/3'>   
+    <div className='flex flex-col items-center justify-center'>   
       <h2 className="text-3xl">There was a problem.</h2>
       <p className='mt-5'>
         Go back to the <Link href="/login" className='text-primary'>Login Page</Link>
