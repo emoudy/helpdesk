01d8a6cfdc8660b22c2436704c22a22d72d48ed9
Elda Moudy <elda@moudy.net>
2024-09-05 09:09:57 -0400

    removing console.log

--- app/(dashboard)/tickets/_helperFunctions/getTickets.ts
+++ app/(dashboard)/tickets/_helperFunctions/getTickets.ts
@@ -9,13 +9,10 @@ interface GetTicketsProps {
 export async function getTickets({ priority = "all", user_email = "" }:GetTicketsProps): Promise<Ticket[]> {
   const supabase = createClient();
   let query = supabase.from('Tickets').select();
-  console.log("getTickets", priority, user_email);
   if (priority !== 'all') query = query.eq('priority', priority);
   if (user_email.trim() !== "") query = query.eq('user_email', user_email);
 
   const { data, error } = await query;
-  console.log("getTickets data", data);
-  console.log("getTickets error", error);
 
   if (error) {
     throw new Error("Error fetching tickets");
